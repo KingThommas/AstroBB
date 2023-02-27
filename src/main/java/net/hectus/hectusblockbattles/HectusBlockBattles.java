b52c6a82471453f2de61474285fcaa8c88defa4e
MarcPG1905 <marcgriepenkerl8@gmail.com>
2023-02-27 16:03:09 +0100

    ok.

---
+++ src/main/java/net/hectus/hectusblockbattles/HectusBlockBattles.java
@@ -0,0 +1,16 @@
+package net.hectus.hectusblockbattles;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HectusBlockBattles extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("Heheheha!");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
