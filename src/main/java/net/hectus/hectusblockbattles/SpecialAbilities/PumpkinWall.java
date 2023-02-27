59b9c4f9cf4e9d6e95d383a9f2b0fc24a73e2a8f
Blejert <hubert1509j@wp.pl>
2023-02-27 20:21:25 +0100

    carved pumpkin wall

--- src/main/java/net/hectus/hectusblockbattles/SpecialAbilities/PumpkinWall.java
+++ src/main/java/net/hectus/hectusblockbattles/SpecialAbilities/PumpkinWall.java
@@ -1,7 +1,7 @@
 package net.hectus.hectusblockbattles.SpecialAbilities;
 
 import org.bukkit.Material;
-import org.bukkit.plugin.java.JavaPlugin;
+
 //probably will need a lot of imports of classes
 //test this
 public class PumpkinWall {
@@ -11,7 +11,7 @@ public class PumpkinWall {
     int step;
     // if returns 0 player misplaced a block and should lose, if returns 1 continue to run this function, if returns 2 player ended placing the pumpkin wall
     public int didPlayerContinuePlacing(int x, int y, int z, Material blockType, boolean facingCenter, int direction) { //direction 0: wall facing positive x; 1: facing negative x; 2: facing pos z; 3: neg z;
-        if(blockType==Material.PUMPKIN) {
+        if(blockType==Material.CARVED_PUMPKIN) {
             if(y==currentY+(step%2==0?0:1)&&x==currentX+(direction>1&&step%2==0?direction==2?1:-1:0)&&z==currentZ+(direction<2?direction==0?1:-1:0)&&facingCenter) {
                 step+=1;
                 if(step==10) {
@@ -24,7 +24,7 @@ public class PumpkinWall {
     }
     //activate when player places a block on their turn, if returns true then go to the opponents turn, instead every time the player places a block run the function above
     public boolean didPlayerStartPlacing(int x, int y, int z, Material blockType, boolean facingCenter) {
-        if(blockType==Material.PUMPKIN&&facingCenter) {
+        if(blockType==Material.CARVED_PUMPKIN&&facingCenter) {
             currentX = x;
             currentY = y;
             currentZ = z;
