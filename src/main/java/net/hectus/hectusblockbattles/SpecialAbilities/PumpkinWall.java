1c323ba98e3b136f57b7f5f0c69d2d4ef4cd6c6c
Blejert <hubert1509j@wp.pl>
2023-02-27 20:33:22 +0100

    even more fixes for carved pumpkin wall

--- src/main/java/net/hectus/hectusblockbattles/SpecialAbilities/PumpkinWall.java
+++ src/main/java/net/hectus/hectusblockbattles/SpecialAbilities/PumpkinWall.java
@@ -9,9 +9,10 @@ public class PumpkinWall {
     int currentY;
     int currentZ;
     int step;
+    int correctDirection;
     // if returns 0 player misplaced a block and should lose, if returns 1 continue to run this function, if returns 2 player ended placing the pumpkin wall
-    public int didPlayerContinuePlacing(int x, int y, int z, Material blockType, boolean facingCenter, int direction) { //direction 0: wall facing positive x; 1: facing negative x; 2: facing pos z; 3: neg z;
-        if(blockType==Material.CARVED_PUMPKIN) {
+    public int didPlayerContinuePlacing(int x, int y, int z, Material blockType, boolean facingCenter, int direction, boolean placedBlockingTheSun) { //direction 0: wall facing positive x; 1: facing negative x; 2: facing pos z; 3: neg z;
+        if(blockType==Material.CARVED_PUMPKIN&&placedBlockingTheSun&&correctDirection==direction) {
             if(y==currentY+(step%2==0?0:1)&&x==currentX+(direction>1&&step%2==0?direction==2?1:-1:0)&&z==currentZ+(direction<2?direction==0?1:-1:0)&&facingCenter) {
                 step+=1;
                 if(step==10) {
@@ -23,12 +24,13 @@ public class PumpkinWall {
         return 0;
     }
     //activate when player places a block on their turn, if returns true then go to the opponents turn, instead every time the player places a block run the function above
-    public boolean didPlayerStartPlacing(int x, int y, int z, Material blockType, boolean facingCenter) {
-        if(blockType==Material.CARVED_PUMPKIN&&facingCenter) {
+    public boolean didPlayerStartPlacing(int x, int y, int z, Material blockType, boolean facingCenter, int direction, boolean placedBlockingTheSun) {
+        if(blockType==Material.CARVED_PUMPKIN&&facingCenter&&placedBlockingTheSun) {
             currentX = x;
             currentY = y;
             currentZ = z;
             step = 1;
+            correctDirection = direction;
             return true;
         }
         return false;
