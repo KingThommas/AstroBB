6f5defa385c0a133b8f8231bbcf3433fc5d4f5dc
PraiseJS <whitestonescontact@gmail.com>
2023-02-27 17:34:35 +0100

    pushin

--- src/main/java/net/hectus/hectusblockbattles/Hub/serverSelector.java
+++ src/main/java/net/hectus/hectusblockbattles/Hub/serverSelector.java
@@ -3,11 +3,10 @@ package net.hectus.hectusblockbattles.Hub;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.jetbrains.annotations.NotNull;
 
 public class serverSelector implements CommandExecutor {
     @Override
-    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         return false;
     }
 }
