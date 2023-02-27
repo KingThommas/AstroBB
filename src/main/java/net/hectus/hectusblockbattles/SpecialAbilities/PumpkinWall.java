12ab7f636e2bb4c9403cf24f171679ce237cb0da
Blejert <hubert1509j@wp.pl>
2023-02-27 21:36:39 +0100

    night

--- src/main/java/net/hectus/hectusblockbattles/SpecialAbilities/PumpkinWall.java
+++ src/main/java/net/hectus/hectusblockbattles/SpecialAbilities/PumpkinWall.java
@@ -1,6 +1,7 @@
 package net.hectus.hectusblockbattles.SpecialAbilities;
 
 import org.bukkit.Material;
+import org.bukkit.World;
 
 //probably will need a lot of imports of classes
 //test this
@@ -11,11 +12,12 @@ public class PumpkinWall {
     int step;
     int correctDirection;
     // if returns 0 player misplaced a block and should lose, if returns 1 continue to run this function, if returns 2 player ended placing the pumpkin wall
-    public int didPlayerContinuePlacing(int x, int y, int z, Material blockType, boolean facingCenter, int direction, boolean placedBlockingTheSun) { //direction 0: wall facing positive x; 1: facing negative x; 2: facing pos z; 3: neg z;
+    public int didPlayerContinuePlacing(int x, int y, int z, Material blockType, boolean facingCenter, int direction, boolean placedBlockingTheSun, World world) { //direction 0: wall facing positive x; 1: facing negative x; 2: facing pos z; 3: neg z;
         if(blockType==Material.CARVED_PUMPKIN&&placedBlockingTheSun&&correctDirection==direction) {
             if(y==currentY+(step%2==0?0:1)&&x==currentX+(direction>1&&step%2==0?direction==2?1:-1:0)&&z==currentZ+(direction<2?direction==0?1:-1:0)&&facingCenter) {
                 step+=1;
                 if(step==10) {
+                    onNight(world);
                     return 2;
                 }
                 return 1;
@@ -34,5 +36,16 @@ public class PumpkinWall {
             return true;
         }
         return false;
+    }
+    public void onNight(World world) {
+        for (int i = 0; i < 48; i++) {
+            for (int j = 0; j < 48; j++) {
+                for (int k = 0; k < 48; k++) {
+                    if(world.getBlockAt(i,j,k).getType()==Material.LIGHT_BLUE_CONCRETE) {
+                        world.getBlockAt(i,j,k).setType(Material.BLACK_CONCRETE);
+                    }
+                }
+            }
+        }
     }
 }
