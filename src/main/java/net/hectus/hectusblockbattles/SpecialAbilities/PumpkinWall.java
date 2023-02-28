1ce0092e34afd481d29e936fb85861e78efd5087
Blejert <hubert1509j@wp.pl>
2023-02-28 16:38:37 +0100

    comments for pumpkinwall

--- src/main/java/net/hectus/hectusblockbattles/SpecialAbilities/PumpkinWall.java
+++ src/main/java/net/hectus/hectusblockbattles/SpecialAbilities/PumpkinWall.java
@@ -12,7 +12,7 @@ public class PumpkinWall {
     int currentZ;
     int step;
     // if returns 0 player misplaced a block and should lose, if returns 1 continue to run this function, if returns 2 player ended placing the pumpkin wall
-    public int didPlayerContinuePlacing(int x, int y, int z, Material blockType, boolean facingCenter, Player player) { //direction 0: wall facing positive x; 1: facing negative x; 2: facing pos z; 3: neg z;
+    public int didPlayerContinuePlacing(int x, int y, int z, Material blockType, boolean facingCenter, Player player) {
         if(blockType == Material.CARVED_PUMPKIN && player.getLocation().getYaw() > 50 && player.getLocation().getYaw() < 140) {
             if(y == currentY + (step%2 == 0?0:1) && x == currentX && z == currentZ + (step%2 == 0?-1:0) && facingCenter) {
                 step += 1;
@@ -25,7 +25,7 @@ public class PumpkinWall {
         }
         return 0;
     }
-    // Activate when player places a block on their turn, if returns true then go to the opponents turn, instead every time the player places a block run the function above
+    //activate when player places a block on their turn, if returns true then don't go to the opponents turn, instead every time the player places a block run the function above to check if the player successfully placed the wall
     public boolean didPlayerStartPlacing(int x, int y, int z, Material blockType, boolean facingCenter, Player player) {
         if(blockType == Material.CARVED_PUMPKIN && facingCenter && player.getLocation().getYaw() > 50 && player.getLocation().getYaw() < 140) {
             currentX = x;
