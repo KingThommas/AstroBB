8bfd38b332bf6d097a0c62d3135697ce43b81ebe
MarcPG1905 <marcgriepenkerl8@gmail.com>
2023-02-28 16:37:14 +0100

    ok.

--- src/main/java/net/hectus/hectusblockbattles/SpecialAbilities/PumpkinWall.java
+++ src/main/java/net/hectus/hectusblockbattles/SpecialAbilities/PumpkinWall.java
@@ -13,10 +13,10 @@ public class PumpkinWall {
     int step;
     // if returns 0 player misplaced a block and should lose, if returns 1 continue to run this function, if returns 2 player ended placing the pumpkin wall
     public int didPlayerContinuePlacing(int x, int y, int z, Material blockType, boolean facingCenter, Player player) { //direction 0: wall facing positive x; 1: facing negative x; 2: facing pos z; 3: neg z;
-        if(blockType==Material.CARVED_PUMPKIN&&player.getLocation().getYaw()>50&&player.getLocation().getYaw()<140) {
-            if(y==currentY+(step%2==0?0:1)&&x==currentX&&z==currentZ+(step%2==0?-1:0)&&facingCenter) {
-                step+=1;
-                if(step==12) {
+        if(blockType == Material.CARVED_PUMPKIN && player.getLocation().getYaw() > 50 && player.getLocation().getYaw() < 140) {
+            if(y == currentY + (step%2 == 0?0:1) && x == currentX && z == currentZ + (step%2 == 0?-1:0) && facingCenter) {
+                step += 1;
+                if(step == 12) {
                     onNight(player.getWorld());
                     return 2;
                 }
@@ -25,9 +25,9 @@ public class PumpkinWall {
         }
         return 0;
     }
-    //activate when player places a block on their turn, if returns true then go to the opponents turn, instead every time the player places a block run the function above
+    // Activate when player places a block on their turn, if returns true then go to the opponents turn, instead every time the player places a block run the function above
     public boolean didPlayerStartPlacing(int x, int y, int z, Material blockType, boolean facingCenter, Player player) {
-        if(blockType==Material.CARVED_PUMPKIN&&facingCenter&&player.getLocation().getYaw()>50&&player.getLocation().getYaw()<140) {
+        if(blockType == Material.CARVED_PUMPKIN && facingCenter && player.getLocation().getYaw() > 50 && player.getLocation().getYaw() < 140) {
             currentX = x;
             currentY = y;
             currentZ = z;
@@ -40,14 +40,14 @@ public class PumpkinWall {
         for (int i = 0; i < 48; i++) {
             for (int j = 0; j < 48; j++) {
                 for (int k = 0; k < 48; k++) {
-                    if(world.getBlockAt(i,j,k).getType()==Material.LIGHT_BLUE_CONCRETE) {
-                        world.getBlockAt(i,j,k).setType(Material.BLACK_CONCRETE);
+                    if(world.getBlockAt(i, j, k).getType() == Material.LIGHT_BLUE_CONCRETE) {
+                        world.getBlockAt(i, j, k).setType(Material.BLACK_CONCRETE);
                     }
-                    if(world.getBlockAt(i,j,k).getType()==Material.BLUE_CONCRETE) {
-                        world.getBlockAt(i,j,k).setType(Material.BLACK_CONCRETE);
+                    if(world.getBlockAt(i, j, k).getType() == Material.BLUE_CONCRETE) {
+                        world.getBlockAt(i, j, k).setType(Material.BLACK_CONCRETE);
                     }
-                    if(world.getBlockAt(i,j,k).getType()==Material.YELLOW_CONCRETE) {
-                        world.getBlockAt(i,j,k).setType(Material.LIGHT_GRAY_CONCRETE);
+                    if(world.getBlockAt(i, j, k).getType() == Material.YELLOW_CONCRETE) {
+                        world.getBlockAt(i, j, k).setType(Material.LIGHT_GRAY_CONCRETE);
                     }
                 }
             }
