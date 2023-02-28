a42c7f9aaa64ef2ec6dafafd3d89d561b036e48b
Blejert <hubert1509j@wp.pl>
2023-02-28 19:51:22 +0100

    glasswalls

---
+++ src/main/java/net/hectus/hectusblockbattles/SpecialAbilities/GlassWalls.java
@@ -0,0 +1,34 @@
+package net.hectus.hectusblockbattles.SpecialAbilities;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+
+public class GlassWalls {
+    int currentX;
+    int currentY;
+    int currentZ;
+    int step;
+    // same as pumpkin wall
+    public Material didPlayerContinuePlacing(int x, int y, int z, Material blockType) {
+        if(blockType == Material.GLASS || blockType.name().startsWith("STAINED_GLASS")) {
+            if(y == currentY + (step % 2 == 0 ? 0 : 1) && ( x == currentX + ( step % 2 == 1 ? 1 : 0 ) ^ z == currentZ + ( step % 2 == 0 ? 1 : 0 ) ^ x == currentX + ( step % 2 == 1 ? -1 : 0 ) ^ z == currentZ + ( step % 2 == 0 ? -1 : 0 ) ) ) {
+                step += 1;
+                if(step == 12) {
+                    return blockType; //player successfully placed the wall and returned the glass block used
+                }
+                return Material.WHITE_CONCRETE; //continue checking
+            }
+        }
+        return Material.RED_CONCRETE; //player failed
+    }
+    public boolean didPlayerStartPlacing(int x, int y, int z, Material blockType) {
+        if(blockType == Material.GLASS || blockType.name().startsWith("STAINED_GLASS")) {
+            currentX = x;
+            currentY = y;
+            currentZ = z;
+            step = 1;
+            return true;
+        }
+        return false;
+    }
+}
