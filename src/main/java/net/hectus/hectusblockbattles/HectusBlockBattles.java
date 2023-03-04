6c71f8467424224344f8b52a5e8e2d075335b343
Blejert <hubert1509j@wp.pl>
2023-03-05 00:34:58 +0100

    warps softcoded

--- src/main/java/net/hectus/hectusblockbattles/HectusBlockBattles.java
+++ src/main/java/net/hectus/hectusblockbattles/HectusBlockBattles.java
@@ -9,6 +9,9 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public final class HectusBlockBattles extends JavaPlugin implements Listener {
     MatchData MD = new MatchData();
     @Override
@@ -48,14 +51,26 @@ public final class HectusBlockBattles extends JavaPlugin implements Listener {
                 Material result = GW.didPlayerContinuePlacingGlass(x, y, z, material);
                 if (result == Material.WHITE_CONCRETE) {
                     return true;
-                } else if (result == Material.RED_CONCRETE) {
-                    return false;
-                } else {
+                } else if (result != Material.RED_CONCRETE) {
                     // do things after a glass wall is placed
                     return true;
                 }
             }
         }
+        else if (material == Material.NETHERRACK || material == Material.SHROOMLIGHT) {
+            if (tjs && warps.didPlayerStartPlacing(x, y, z, material, player)) {
+                return true;
+            } else if (!tjs) {
+                Material[] result = warps.didPlayerContinuePlacing(x,y,z,material,player);
+                if (Arrays.equals(result, new Material[]{Material.WHITE_CONCRETE})) {
+                    return true;
+                } else if (!Arrays.equals(result, new Material[]{Material.RED_CONCRETE})) {
+                    //do things after nether warp is placed
+                    return true;
+                }
+            }
+        }
+        MD.setVariables(MV, world);
         return false;
     }
 }
