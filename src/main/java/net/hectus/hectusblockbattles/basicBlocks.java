e2f7a0fad07719b32f35350b0d4ee07d3d322f97
unusualotter <46056832+unusualotter@users.noreply.github.com>
2023-03-06 10:38:02 -0500

    Added Material.WET_SPONGE to block group 2

--- src/main/java/net/hectus/hectusblockbattles/basicBlocks.java
+++ src/main/java/net/hectus/hectusblockbattles/basicBlocks.java
@@ -4,13 +4,13 @@ import org.bukkit.Material;
 
 public class basicBlocks {
     private final Material[] groupBlocks = {Material.NETHER_WART_BLOCK, Material.NETHERRACK, Material.NETHER_GOLD_ORE, Material.NETHER_BRICKS, Material.RED_NETHER_BRICKS, Material.NETHERITE_BLOCK, //the first block of a group has a special ability, here it is removing opponents luck boost
-                                            Material.SEA_LANTERN, Material.PRISMARINE, Material.PRISMARINE_BRICKS, Material.DARK_PRISMARINE, null, null, //tells the player who's winning
+                                            Material.SEA_LANTERN, Material.PRISMARINE, Material.PRISMARINE_BRICKS, Material.DARK_PRISMARINE, Material.WET_SPONGE, null, //tells the player who's winning
                                             Material.STONE_BRICKS, Material.STONE, Material.COBBLESTONE, Material.ANDESITE, Material.DIORITE, Material.GRANITE, //
                                             Material.WHITE_SHULKER_BOX, Material.SNOW_BLOCK, Material.WHITE_CONCRETE, Material.LIGHT_GRAY_CONCRETE, Material.GRAY_CONCRETE, Material.BLACK_WOOL,
                                             Material.SNOW_BLOCK, Material.ICE, Material.PACKED_ICE, Material.BLUE_ICE, null, null,
                                             Material.OAK_WOOD, Material.DIRT, Material.MOSS_BLOCK, Material.SMALL_DRIPLEAF, Material.BIG_DRIPLEAF, null};
     private final double[] groupPowers = {2.0, 1.0, 1.5, 2.0, 2.5, 3.2,
-                                          1.0, 1.5, 2.0, 2.5, 0.0, 0.0,
+                                          1.0, 1.5, 2.0, 2.5, 1.5, 0.0,
                                           2.5, 1.5, 2.0, 2.5, 3.0, 0.0,
                                           2.0, 1.0, 1.5, 2.0, 2.5, 3.2,
                                           1.5, 1.5, 2.0, 2.5, 0.0, 0.0,
