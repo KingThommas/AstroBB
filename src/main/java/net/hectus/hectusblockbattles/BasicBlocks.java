8951560c322fb7ebc24e8b467051b97bf69dc3c8
Xaron11 <kaczmarzykdawid4@gmail.com>
2023-04-01 18:46:25 +0200

    Suggestion for general changes

--- src/main/java/net/hectus/hectusblockbattles/BasicBlocks.java
+++ src/main/java/net/hectus/hectusblockbattles/BasicBlocks.java
@@ -1,27 +1,28 @@
 package net.hectus.hectusblockbattles;
 
+import net.hectus.hectusblockbattles.Warps.Warp;
 import org.bukkit.Material;
 
-public class BasicBlocks {
-    private final Material[] groupBlocks = {Material.NETHER_WART_BLOCK, Material.NETHERRACK, Material.NETHER_GOLD_ORE, Material.NETHER_BRICKS, Material.RED_NETHER_BRICKS, Material.NETHERITE_BLOCK, //the first block of a group has a special ability, here it is removing opponents luck boost
+public final class BasicBlocks {
+    private static final Material[] groupBlocks = {Material.NETHER_WART_BLOCK, Material.NETHERRACK, Material.NETHER_GOLD_ORE, Material.NETHER_BRICKS, Material.RED_NETHER_BRICKS, Material.NETHERITE_BLOCK, //the first block of a group has a special ability, here it is removing opponents luck boost
                                             Material.SEA_LANTERN, Material.WET_SPONGE, Material.PRISMARINE, Material.PRISMARINE_BRICKS, Material.DARK_PRISMARINE, null, //tells the player who's winning
                                             Material.STONE_BRICKS, Material.STONE, Material.COBBLESTONE, Material.ANDESITE, Material.DIORITE, Material.GRANITE, //
                                             Material.WHITE_SHULKER_BOX, Material.SNOW_BLOCK, Material.WHITE_CONCRETE, Material.LIGHT_GRAY_CONCRETE, Material.GRAY_CONCRETE, Material.BLACK_WOOL,
                                             Material.SNOW_BLOCK, Material.ICE, Material.PACKED_ICE, Material.BLUE_ICE, null, null,
                                             Material.OAK_WOOD, Material.DIRT, Material.MOSS_BLOCK, Material.SMALL_DRIPLEAF, Material.BIG_DRIPLEAF, null};
-    private final double[] groupPowers = {2.0, 1.0, 1.5, 2.0, 2.5, 3.2,
+    private static final double[] groupPowers = {2.0, 1.0, 1.5, 2.0, 2.5, 3.2,
                                           1.0, 1.5, 2.0, 2.5, 3.0, 0.0,
                                           2.5, 1.5, 2.0, 2.5, 3.0, 0.0,
                                           2.0, 1.0, 1.5, 2.0, 2.5, 3.2,
                                           1.5, 1.5, 2.0, 2.5, 0.0, 0.0,
                                           2.0, 1.0, 1.5, 2.0, 3.0, 0.0};
-    private final int[] groupCounters = {4,5,-1,-1,
+    private static final int[] groupCounters = {4,5,-1,-1,
                                          0,2,-1,-1,
                                          3,0,-1,-1,
                                          5,1,-1,-1,
                                          1,3,-1,-1,
                                          2,4,-1,-1}; //first 4 decide the counters of the first group the next 4 decide the counters of the second group etc.
-    public double calculateGameScore(double currentGameScore, Material material, Material lastBlock, boolean turn, double[] blockBoosts) {
+    public static double calculateGameScore(double currentGameScore, Material material, Material lastBlock, boolean turn, double[] blockBoosts) {
         double power = -1;
         int group = -1;
         int counterGroup = -1;
@@ -51,14 +52,14 @@ public class BasicBlocks {
         return isCurrentPlayerLosing ? currentGameScore * 1.5 : currentGameScore;
     }
 
-    public boolean blockCheck(Material material, String warp, boolean night) {
+    public static boolean blockCheck(Material material, Warp warp, boolean night) {
         int group = -1;
         for (int i = 0; i < groupBlocks.length; i++) {
             if(groupBlocks[i]==material) {
                 group = (int) Math.floor((i+1)/6);
             }
         }
-        if(night && group == 3) {
+        if (night && group == 3) {
             return false;
         }
         return true;
