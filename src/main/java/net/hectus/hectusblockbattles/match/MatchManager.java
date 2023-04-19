adbc97ace9b8903225c1fc9acde9a99264c686d6
chimkenu <106415170+chimkenu@users.noreply.github.com>
2023-04-19 17:03:44 +0800

    Reworked MatchManager

--- src/main/java/net/hectus/hectusblockbattles/match/MatchManager.java
+++ src/main/java/net/hectus/hectusblockbattles/match/MatchManager.java
@@ -2,21 +2,20 @@ package net.hectus.hectusblockbattles.match;
 
 import org.bukkit.World;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 public final class MatchManager {
+    private static final Set<Match> matches = new HashSet<>();
 
-    private static final Map<UUID, Match> worldMatchMap = new HashMap<>();
     public static Match getMatch(World world) {
-        UUID key = world.getUID();
-
-        return worldMatchMap.get(key);
+        for (Match match : matches) {
+            if (match.getGameMap().getWorld() == world)
+                return match;
+        }
+        return null;
     }
 
-    public static void addMatch(Match match, World world) {
-        UUID key = world.getUID();
-        worldMatchMap.put(key, match);
+    public static void addMatch(Match match) {
+        matches.add(match);
     }
 }
