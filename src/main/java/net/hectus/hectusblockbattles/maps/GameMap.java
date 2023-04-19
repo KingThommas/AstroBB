cbf28acdde67f40b084f51ff1b882276d97ce753
chimkenu <106415170+chimkenu@users.noreply.github.com>
2023-04-19 14:41:27 +0800

    Added GameMap

---
+++ src/main/java/net/hectus/hectusblockbattles/maps/GameMap.java
@@ -0,0 +1,12 @@
+package net.hectus.hectusblockbattles.maps;
+
+import org.bukkit.World;
+
+public interface GameMap {
+    boolean load();
+    void unload();
+    boolean restoreFromSource();
+
+    boolean isLoaded();
+    World getWorld();
+}
