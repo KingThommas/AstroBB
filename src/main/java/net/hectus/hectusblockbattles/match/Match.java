a7b3a53acb7f88e26a4ae16be89ed3d830b83f8b
chimkenu <106415170+chimkenu@users.noreply.github.com>
2023-04-19 14:28:37 +0800

    Reworked Match

--- src/main/java/net/hectus/hectusblockbattles/match/Match.java
+++ src/main/java/net/hectus/hectusblockbattles/match/Match.java
@@ -1,14 +1,17 @@
 package net.hectus.hectusblockbattles.match;
 
+import net.hectus.hectusblockbattles.maps.GameMap;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 
-import java.util.Set;
+import java.util.List;
 
 public interface Match {
     boolean start();
     void stop(boolean isAbrupt);
-    World getWorld();
-    Set<Player> getPlayers();
+    public Player getCurrentTurnPlayer();
+    public void nextTurn();
+    GameMap getGameMap();
+    List<Player> getPlayers();
     boolean isRunning();
 }
